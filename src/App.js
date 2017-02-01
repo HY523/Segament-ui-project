8020c457a308a5e077f2b2268d7a0a495f4d8092
Hideki Yamamoto <hideki.y523@outlook.com>
2017-02-02 05:04:12 +0900

    Implement “Container text” component feature

--- src/App.js
+++ src/App.js
@@ -56,19 +56,19 @@ class App extends Component {
               </div>
             </Container>
 
-            <div class="ui text container">
+            <Container text>
               <h1 class="ui inverted header">
                 Imagine-a-Company
               </h1>
               <h2>Do whatever you want when you want to.</h2>
               <div class="ui huge primary button">Get Started <i class="right arrow icon"></i></div>
-            </div>
+            </Container>
 
           </div>
 
 
           <div class="ui vertical stripe segment">
-            <div class="ui text container">
+            <Container text>
               <h3 class="ui header">Breaking The Grid, Grabs Your Attention</h3>
               <p>Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.</p>
               <a class="ui large button">Read More</a>
@@ -78,7 +78,7 @@ class App extends Component {
               <h3 class="ui header">Did We Tell You About Our Bananas?</h3>
               <p>Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really true. It took years of gene splicing and combinatory DNA research, but our bananas can really dance.</p>
               <a class="ui large button">I'm Still Quite Interested</a>
-            </div>
+            </Container>
           </div>
 
 
