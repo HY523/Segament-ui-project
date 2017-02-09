c8c93985a14f7ebf05caf7ad43807783458a3c53
Hideki Yamamoto <hideki.y523@outlook.com>
2017-02-10 04:55:01 +0900

    implement style & responsive about  pointing menu

--- src/components/PointingMenu.js
+++ src/components/PointingMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../App.css'
 import { Menu, Segment, Button, Container, Icon, Header} from 'semantic-ui-react'
 
 export default class PointingMenu extends Component {
@@ -13,20 +14,21 @@ export default class PointingMenu extends Component {
       <Segment inverted>
         <Menu inverted pointing secondary>
           <Container>
-            {/*
-              <div>
-                <Icon disabled name='sidebar'/>
-              </div>
-            */}
-            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-            <Menu.Item name='work' active={activeItem === 'work'} onClick={this.handleItemClick} />
-            <Menu.Item name='company' active={activeItem === 'company'} onClick={this.handleItemClick} />
-            <Menu.Item name='careers' active={activeItem === 'careers'} onClick={this.handleItemClick} />
-          </Container>
 
-          <Container>
-            <Button inverted>Login</Button>
-            <Button inverted>Sign Up</Button>
+              <div className="toc item">
+                <Icon disabled name='sidebar' link/>
+              </div>
+              <div className="pointing_menu">
+                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+                <Menu.Item name='work' active={activeItem === 'work'} onClick={this.handleItemClick}/>
+                <Menu.Item name='company' active={activeItem === 'company'} onClick={this.handleItemClick}/>
+                <Menu.Item name='careers' active={activeItem === 'careers'} onClick={this.handleItemClick}/>
+              </div>
+              <div className="pointing_sign_menu">
+                <Button inverted>Login</Button>
+                <Button inverted>Sign Up</Button>
+              </div>
+
           </Container>
         </Menu>
       </Segment>
