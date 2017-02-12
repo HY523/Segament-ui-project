e2e86af532bf53505164fb7f34a8bad3a7ac5c35
Hideki Yamamoto <hideki.y523@outlook.com>
2017-02-13 05:03:28 +0900

    improve the pointing menu style

--- src/components/PointingMenu.js
+++ src/components/PointingMenu.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import '../App.css'
 import { Menu, Segment, Button, Container, Icon} from 'semantic-ui-react'
-
 export default class PointingMenu extends Component {
   state = { activeItem: 'home' }
 
@@ -12,28 +11,23 @@ export default class PointingMenu extends Component {
 
     return (
       <Segment inverted>
-        <Menu inverted pointing secondary>
+        <Menu inverted pointing secondary size='large'>
           <Container>
-
-              {/*
-                <div className="toc item">
-                  <Icon disabled name='sidebar' link/>
-                </div>
-              */}
-              <div className="toc item">
+            <div className='toc_item'>
+              <Menu.Item name='home'>
                 <Icon disabled name='sidebar' link/>
-              </div>
-              <div className="pointing_menu">
-                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
-                <Menu.Item name='work' active={activeItem === 'work'} onClick={this.handleItemClick}/>
-                <Menu.Item name='company' active={activeItem === 'company'} onClick={this.handleItemClick}/>
-                <Menu.Item name='careers' active={activeItem === 'careers'} onClick={this.handleItemClick}/>
-              </div>
-              <div className="pointing_sign_menu">
-                <Button inverted>Login</Button>
-                <Button inverted>Sign Up</Button>
-              </div>
-
+              </Menu.Item>
+            </div>
+            <div className="pointing_menu">
+              <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+              <Menu.Item name='work' active={activeItem === 'work'} onClick={this.handleItemClick}/>
+              <Menu.Item name='company' active={activeItem === 'company'} onClick={this.handleItemClick}/>
+              <Menu.Item name='careers' active={activeItem === 'careers'} onClick={this.handleItemClick}/>
+            </div>
+            <div className="pointing_sign_menu">
+              <Button inverted>Login</Button>
+              <Button inverted>Sign Up</Button>
+            </div>
           </Container>
         </Menu>
       </Segment>
